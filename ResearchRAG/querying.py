8270f7f07277add61303b6cc05f5bc28e09a8347
aks1708 <aksinghal1708@gmail.com>
2025-09-28 19:56:09 +0530

    Updated requirements.txt and removed Gemma3:1b

--- ResearchRAG/querying.py
+++ ResearchRAG/querying.py
@@ -3,7 +3,6 @@ from typing import List
 
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_ollama import ChatOllama
 
 import reranking
 
@@ -21,29 +20,29 @@ rag_prompt = ChatPromptTemplate.from_messages(
         """You are provided with the following context.
         Context: {context}
         
-        Use the context to give a comprehensive answer to the user's query.
+        ONLY use the context to give a comprehensive answer to the user's query.
+        Do not use your own knowledge to answer the question.
         Only if the given context does not answer the question then say 'I don't know'.
         Do not include phrases such as `according to the context` or `based on the context`. Just give the answer."""),
         ("human", "{query}")
 
     ])
 
-query_expander_llm = ChatOllama(model="llama3.1:8b", temperature=0).bind_tools([QueryExpansion])
+llm = ChatOllama(model="llama3.1:8b", temperature=0)
+query_expander = llm.bind_tools([QueryExpansion])
 
-gemma = ChatOllama(model="gemma3:1b", temperature=0)
+rag_chain = rag_prompt | llm
 
-rag_chain = rag_prompt | gemma
-
-def query_expander(query):
-    queries = query_expander_llm.invoke(query_expansion_prompt.format(query=query))
+def expand_query(query):
+    queries = query_expander.invoke(query_expansion_prompt.format(query=query))
     augmented_queries = [query] + queries.tool_calls[0]['args']['variations']
     return augmented_queries
 
-def get_relevant_docs(augmented_queries, collection, k=10):
+def get_relevant_docs(augmented_queries, collection, k=15):
     return collection.query(query_texts=augmented_queries, n_results=k)['documents'][0]
 
 def generate_response(query, collection):
-    augmented_queries = query_expander(query)
+    augmented_queries = expand_query(query)
     draft_documents = get_relevant_docs(augmented_queries, collection)
 
     refined_context = reranking.reranked_context(query, draft_documents)
